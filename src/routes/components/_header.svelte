ba6cd7ebd95f2fabe8ccff937346e543f661b428
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-22 21:18:20 +0100

    :art: update header logo text from 'Fs' to 'CodeJournal'

--- src/routes/components/_header.svelte
+++ src/routes/components/_header.svelte
@@ -4,7 +4,7 @@
 
 <header class="min-w-full p-4 md:p-8">
 	<nav class="flex justify-around">
-		<a href="{base}/" class="logo">Fs</a>
+		<a href="{base}/" class="logo">CodeJournal</a>
 		<ul class="flex gap-4">
 			<li class="mr-2 hidden hover:underline md:block">
 				<a href="{base}/">Home</a>
